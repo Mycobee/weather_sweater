f93da8f4ff3b9e3b8cdfe550bfc1854ed4076e46
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-29 11:29:19 -0600

    Edits tests to be more meaningful, pivoting to format json instead of cleaning up and adding more specs, per josh's request

--- spec/api/v1/munchies_request_spec.rb
+++ spec/api/v1/munchies_request_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 describe "I send a request to munchies api with trip route and food desired" do
-		before :each do
-		
-		end
-		
-		it "returns all the shops of that flavor that will be open" do
-			get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"	
+	it "returns all the shops of that flavor that will be open" do
+		get "/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese"	
 
-			expect(response).to be_successful
-		end
+		expect(response).to be_successful
+		
+		results = JSON.parse(response.body)
+		expect(results.count).to eq(3)
+		expect(results[0]).to have_key("name")
+		expect(results[0]).to have_key("address")
+	end
 end
 
