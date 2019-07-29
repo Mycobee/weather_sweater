54027f8322cfa2297caa09abec203e4b76ee13d4
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-28 19:26:51 -0600

    Creates initial forecast_generator_spec.rb

--- spec/models/forecast_generator_spec.rb
+++ spec/models/forecast_generator_spec.rb
@@ -4,16 +4,15 @@ describe ForecastGenerator, type: :model do
 	describe "" do
 		before :each do
 			attrs = { "location"=>"denver,co" }
-			@generator = ForecastGenerator.new(attrs)	
+			@generator = ForecastGenerator.new(attrs)
 		end
 		
 		it "exists" do
-			expect(subject).to be_a ForecastGenerator	
+			expect(subject).to be_a ForecastGenerator
 		end
 
-		it "creates a location" do
-			expect(@generator.location).to eq("denver,co")
+		it "generates a forecast hash" do
+			expect(@generator.get_forecast).to eq("denver,co")
 		end
 	end
 end
-
