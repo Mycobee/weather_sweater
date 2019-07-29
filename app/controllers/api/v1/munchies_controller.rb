e8c8927564c322d53d83b7ad6ab5772f09b68af1
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-29 10:04:15 -0600

    still working on yelp call

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::MunchiesController < ApplicationController
 	def index
 	 trip_time = GoogleDirectionsService.new(params).get_trip[:routes][0][:legs][0][:duration][:value]
-	# do the yelp things??
-	#
-		conn = Faraday.new('yelp') do |faraday|
+ binding.pry
+		conn = Faraday.new('https://api.yelp.com/') do |faraday|
 			faraday.adapter Faraday.default_adapter
 			faraday.headers['Authorization'] = 'Bearer ' + ENV['YELP_API_TOKEN']
 		end
 		
-		
+		x = conn.get do |req|
+			req.url '/v3/businesses/search'
+			req.params['location'] = params[:end]
+			req.params['open_at'] =	Time.now.to_i + (trip_time * 1000) 
+		end	
 	end
 end
 
