409b2e5223c247da57cfad77e7f4e20cc2e7c7dc
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-29 10:24:41 -0600

    Got working request returning all needed data to complete story

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,16 +1,23 @@
 class Api::V1::MunchiesController < ApplicationController
 	def index
 	 trip_time = GoogleDirectionsService.new(params).get_trip[:routes][0][:legs][0][:duration][:value]
- binding.pry
-		conn = Faraday.new('https://api.yelp.com/') do |faraday|
+		conn = Faraday.new('https://api.yelp.com') do |faraday|
 			faraday.adapter Faraday.default_adapter
 			faraday.headers['Authorization'] = 'Bearer ' + ENV['YELP_API_TOKEN']
 		end
 		
-		x = conn.get do |req|
+		response = conn.get do |req|
 			req.url '/v3/businesses/search'
 			req.params['location'] = params[:end]
-			req.params['open_at'] =	Time.now.to_i + (trip_time * 1000) 
+			req.params['open_at'] =	Time.now.to_i + trip_time.seconds  
+			req.params['categories'] = params['food']
+		end	
+	
+		business_data = JSON.parse(response.body, symbolize_names: true)
+		### dream a poro to do the logic sifting BusinessPlucker.new(business_data)
+		 binding.pry 
+		0.upto(2).each do |index|
+
 		end	
 	end
 end
