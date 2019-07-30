99a369e1c350ba30d8fdded8abf8f3aeb058a9a0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-29 19:13:22 -0600

    Rewords spec for clarity

--- spec/api/v1/city_weather_request_spec.rb
+++ spec/api/v1/city_weather_request_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe 'forecast api' do
-  it 'sends the forecast for my city' do
+  it 'sends the forecast for requested city' do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
