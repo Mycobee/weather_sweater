f77ddaa1f7a3161000a5bafcad3a673019d4545a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-30 17:36:29 -0600

    Adds spec for user account creation

--- spec/api/v1/user_can_create_account_spec.rb
+++ spec/api/v1/user_can_create_account_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
-describe "create account api" do
-	it "it allows a user to create an acct" do
-		post '/api/v1/users?email=whatever@example.com&password=password&password_confirmation=password'			
-	
+describe "create account api"do
+	it "it allows a user to create an acct" , type: :request do
+		user = {
+			"email": "whatever@example.com", 
+			"password": "password",
+			"password_confirmation": "password"
+		}
+
+		post '/api/v1/users', params: user
 		expect(response).to be_successful
 		key = JSON.parse(response.body)	
 		user = User.last	
