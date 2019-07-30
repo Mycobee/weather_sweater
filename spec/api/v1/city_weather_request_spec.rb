02f3bb51a505658f8dd5cdf369f296c3a3d3d33d
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-29 19:42:36 -0600

    Adds vcr to city_weather_request spec

--- spec/api/v1/city_weather_request_spec.rb
+++ spec/api/v1/city_weather_request_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 describe 'forecast api' do
   it 'sends the forecast for requested city' do
-    get '/api/v1/forecast?location=denver,co'
+		VCR.use_cassette('city_weather_request') do
+    	get '/api/v1/forecast?location=denver,co'
 
-    expect(response).to be_successful
-		
-		forecast = JSON.parse(response.body)
-		#5 day forecast
-		#daily high and low for every day
+    	expect(response).to be_successful
+			
+			forecast = JSON.parse(response.body)
+		end
   end
 end
