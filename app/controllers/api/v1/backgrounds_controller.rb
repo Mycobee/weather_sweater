42d7f3e80ad76cdfea8b88fd1fd3bf9cafd6a15f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-30 11:50:06 -0600

    Refactors controller to call facade and removes faraday

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
 	def index
-
+		photo_facade = PhotoFacade.new(params).format_photos	
+		render json: photo_facade.to_json 
 	end
 end
 
