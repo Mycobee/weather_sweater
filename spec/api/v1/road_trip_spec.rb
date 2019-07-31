52a4b7cee4a6dafe7387f55115c79cdeadb7b8aa
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-31 09:52:21 -0600

    adds road trips spec

---
+++ spec/api/v1/road_trip_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe "road trip api" do
+		xit "takes a start and end destination and returns weather for that time" do
+			VCR.use_cassette('authorized_road_trip', allow_playback_repeats: true) do
+				api_key = ApiKey.new.key
+				user = User.create(email: "whatever@example.com", password: 'password', api_key: api_key)
+				road_trip = {
+  				"origin": "Denver,CO", 
+  				"destination": "Pueblo,CO",
+  				"api_key": api_key
+				}
+
+				headers = {
+      		'CONTENT_TYPE' => 'application/json',
+      		'ACCEPT' => 'application/json'
+    		}
+
+				post "/api/v1/road_trip", params: road_trip.to_json, headers: headers
+				data = JSON.parse(response.body)["data"]
+				expect(data["summary"]).to eq("Partly Cloudy")
+				expect(data["expected_temp"]).to eq(93.45)
+				expect(data["travel_time"]).to eq(data)
+			end
+		end
+
+		it "sends a 401 with bad api_key" do
+			WebMock.allow_net_connect!
+			VCR.turn_off!
+			api_key = ApiKey.new.key
+			user = User.create(email: "whatever@example.com", password: 'password', api_key: 123456)
+			road_trip = {
+  			"origin": "Denver,CO", 
+  			"destination": "Pueblo,CO",
+  			"api_key": api_key
+			}
+
+			headers = {
+      	'CONTENT_TYPE' => 'application/json',
+      	'ACCEPT' => 'application/json'
+    	}
+
+			post "/api/v1/road_trip", params: road_trip.to_json, headers: headers
+			errors = JSON.parse(response.body)["errors"]
+			expect(errors[0]["status"]).to eq("401")
+			expect(errors[0]["title"]).to eq("Unauthorized")
+		end
+end
+
