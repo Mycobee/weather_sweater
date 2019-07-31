2815ead5dd5ea94459fc0230d9f9a01bcd7742a7
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-31 09:47:13 -0600

    Adds methods for future forecasts

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,7 +1,6 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 	attributes :complete_forecast do |forecast|
-
 		{data: [forecast.current_weather, 
 						forecast.today_details,
 						forecast.daily_forecasts,
