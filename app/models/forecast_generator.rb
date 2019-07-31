ef0c846090baa905264f3db1fd7ab665abb9fee7
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-31 14:40:38 -0600

    adds caching to project"

--- app/models/forecast_generator.rb
+++ app/models/forecast_generator.rb
@@ -4,18 +4,22 @@ class ForecastGenerator
 	end
 
 	def get_forecast
-		coords = GoogleGeocodingService.new(@params).get_coords
-		forecast_data = DarkSkyService.new(coords).get_forecast
-		Forecast.new(forecast_data)
+		Rails.cache.fetch("current_forecast", expires_in: 15.minutes) do
+			coords = GoogleGeocodingService.new(@params).get_coords
+			forecast_data = DarkSkyService.new(coords).get_forecast
+			Forecast.new(forecast_data)
+		end
 	end
 
 	def get_future_forecast(seconds)
-		coords = GoogleGeocodingService.new(@params).get_coords
-		future_cast = DarkSkyService.new(coords).get_future_forecast(seconds)
-		forecast = Forecast.new(future_cast)
-		{ data: { expected_temp: forecast.current_temp, 
-			summary: forecast.current_summary, 
-			travel_time: (Time.at(seconds).utc.strftime("%H:%M:%S")) }}.to_json	
+		Rails.cache.fetch("future_forecast", expires_in: 15.minutes) do
+			coords = GoogleGeocodingService.new(@params).get_coords
+			future_cast = DarkSkyService.new(coords).get_future_forecast(seconds)
+			forecast = Forecast.new(future_cast)
+			{ data: { expected_temp: forecast.current_temp, 
+				summary: forecast.current_summary, 
+				travel_time: (Time.at(seconds).utc.strftime("%H:%M:%S")) }}.to_json
+		end
 	end
 end
 
